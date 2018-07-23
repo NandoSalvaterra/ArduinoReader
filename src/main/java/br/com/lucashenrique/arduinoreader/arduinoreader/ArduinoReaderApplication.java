4d9975ea7797d8b494c38c4576e59a94a6b69ee2
Luiz Fernando Salvaterra <luizfernandosalvaterra@gmail.com>
2018-07-23 15:51:26 -0300

    First Commit

---
+++ src/main/java/br/com/lucashenrique/arduinoreader/arduinoreader/ArduinoReaderApplication.java
@@ -0,0 +1,12 @@
+package br.com.lucashenrique.arduinoreader.arduinoreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArduinoReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArduinoReaderApplication.class, args);
+    }
+}
---
+++ src/main/java/br/com/lucashenrique/arduinoreader/arduinoreader/controller/MainController.java
@@ -0,0 +1,7 @@
+package br.com.lucashenrique.arduinoreader.arduinoreader.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainController {
+}
